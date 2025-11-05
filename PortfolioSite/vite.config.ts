44f8e9c5bc30bad2e151e0d78c57e30e199cd949
adbhati05 <bhati.18@buckeyemail.osu.edu>
2025-11-05 18:32:17 -0500

    Declared a new alias, '@', for the ./src path to make importing files easier.

--- PortfolioSite/vite.config.ts
+++ PortfolioSite/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(), 
     tailwindcss()],
-})
+    resolve: {
+      // Essentially, every time I import a component or file under the src directory, I can use the alias "@" instead of writing out the full path.
+      alias: {
+        "@": path.resolve(__dirname, './src')
+      },
+    },
+});
