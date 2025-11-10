0e9c3ba42272be5f5d3cdd102bb24128c0948921
adbhati05 <bhati.18@buckeyemail.osu.edu>
2025-11-10 17:51:00 -0500

    Laid down the foundational styling for the site in index.css and got started working on the Home page.

--- PortfolioSite/src/pages/Home.tsx
+++ PortfolioSite/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 export const Home = () => {
-    return <div className="min-h-screen flex ">
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         {/* Theme Toggle */}
 
         {/* Background Effects */}
