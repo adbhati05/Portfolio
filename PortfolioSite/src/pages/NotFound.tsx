6c9e2659db4421c69adf0d58709a951a88fc4c13
adbhati05 <bhati.18@buckeyemail.osu.edu>
2025-11-22 16:51:26 -0500

    Added TO-DO for this page, deploying site for now, will come back to this later.

--- PortfolioSite/src/pages/NotFound.tsx
+++ PortfolioSite/src/pages/NotFound.tsx
@@ -1,4 +1,5 @@
 const NotFound = () => {
+    // TO-DO: Style this page to match the rest of the portfolio site. And ensure there's a link back to the home page.
     return <div>404 - Page Not Found</div>;
 };
 
